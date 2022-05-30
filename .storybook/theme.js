890512ce635bff28aab309fc836b9981cf9fc459
ashishb <ashishb@geekyants.com>
2022-05-30 16:36:47 +0530

    updated theming

---
+++ .storybook/theme.js
@@ -0,0 +1,9 @@
+import { create } from '@storybook/theming';
+
+export default create({
+  base: 'light',
+  brandTitle: "Partner's",
+  brandUrl: 'https://topgeek.io/',
+  brandImage: 'https://topgeek.io/assets/images/hlogo.svg',
+  brandTarget: '_self',
+});
